02ef526a9349143d2447078fdf0fd980a1beedec
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2021-04-06 17:46:28 +0300

    File upload logic.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
+var fileUpload = require("express-fileupload");
+var cors = require("cors");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
@@ -7,11 +10,28 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// TODO: make async?
+try {
+  fs.mkdirSync(`${__dirname}/sessions`);
+} catch (error) {
+  if (error.code == "EEXIST") {
+    console.log("Session folder exists.");
+  } else {
+    console.log(error);
+  }
+}
 
 app.use("/", indexRouter);
 
