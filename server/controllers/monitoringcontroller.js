cd8d0871a2d87b2f534f97cc6f1f945461c1670b
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2021-05-06 23:11:46 +0300

    Replace face-api.js with maintained version, test.

--- server/controllers/monitoringcontroller.js
+++ server/controllers/monitoringcontroller.js
@@ -1,4 +1,79 @@
+// TODO: test ES6 imports
+
+const tf = require("@tensorflow/tfjs-node");
+
 var fs = require("fs");
+var path = require("path");
+
+// var { Canvas, Image, ImageData, loadImage } = require("canvas");
+// var faceapi = require("@vladmandic/face-api");
+
+// faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+
+// // TODO: consolidate in functions
+
+// console.log("Loading...");
+
+// Promise.all([
+//   faceapi.nets.tinyFaceDetector.loadFromDisk("./public/models"),
+//   faceapi.nets.faceLandmark68TinyNet.loadFromDisk("./public/models"),
+// ])
+//   .then(
+//     async () => {
+//       console.log("Loaded!");
+
+//       try {
+//         const img = await loadImage(
+//           "./sessions/MIRCEACRISTIAN BALOMIREAN/1620324485940.png"
+//         );
+//         const results = await faceapi
+//           .detectAllFaces(
+//             img,
+//             new faceapi.TinyFaceDetectorOptions({
+//               inputSize: 320,
+//               scoreThreshold: 0.5,
+//             })
+//           )
+//           .withFaceLandmarks(true);
+
+//         console.log(results);
+
+//         const out = faceapi.createCanvasFromMedia(img);
+//         faceapi.draw.drawDetections(
+//           out,
+//           results.map((result) => result.detection)
+//         );
+//         faceapi.draw.drawFaceLandmarks(
+//           out,
+//           results.map((result) => result.landmarks),
+//           { drawLines: true, color: "red" }
+//         );
+
+//         const baseDir = path.resolve(__dirname, "./out");
+
+//         if (!fs.existsSync(baseDir)) {
+//           fs.mkdirSync(baseDir);
+//         }
+//         // this is ok for prototyping but using sync methods
+//         // is bad practice in NodeJS
+//         fs.writeFileSync(
+//           path.resolve(baseDir, "test.png"),
+//           out.toBuffer("image/png")
+//         );
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     },
+//     (reason) => {
+//       console.log("Not loaded!");
+//       console.error(reason);
+//     }
+//   )
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+function analyzeData() {}
 
 // TODO: validate, verify files
 // TODO: decide path based on student
